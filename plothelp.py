6254ec43f36f3f270c5535ec798f80dee1e52023
Daniel James Korchinski <danielkorchinski@gmail.com>
2024-11-25 12:39:02 +0100

    removed binning code from plothelp, and moved get_param_array_from_dictionaries into dict_utils.

--- plothelp.py
+++ plothelp.py
@@ -113,57 +113,6 @@ def histvar(binedges,density=None,counts=None,excludeUpperInteger=False,geometri
     sqmean = np.nansum((binedges[1:]-binedges[:-1])*density*(binC**2))
     return sqmean - mean**2 
 
-def binnedavg(tbins,times,quantity,geomean=False):
-    counts,wcounts,w2counts = binnedavg_accumulate(tbins,times,quantity,geomean=geomean)
-    return binnedavg_compute(counts,wcounts,w2counts,geomean=geomean)
-
-def binnedavg_accumulate(tbins,times,quantity,counts=None,wcounts=None,w2counts=None,geomean:bool =False ):
-    q_use = quantity 
-    if(geomean):
-        np.log(quantity)
-    counts_temp,_ = np.histogram(times,bins=tbins)
-    wcounts_temp,_ = np.histogram(times,bins=tbins,weights=q_use)
-    w2counts_temp,_ = np.histogram(times,bins=tbins,weights=q_use**2)
-    
-    if(not(counts is None)):
-        counts+=counts_temp 
-        wcounts+=wcounts_temp 
-        w2counts+=w2counts_temp 
-        return counts,wcounts,w2counts
-    else:
-        return counts_temp,wcounts_temp,w2counts_temp
-
-def binnedavg_compute(counts,wcounts,w2counts,geomean:bool=False):    
-    filt = counts>0
-
-    wcounts[filt]=wcounts[filt]/counts[filt]
-    w2counts[filt] = w2counts[filt]/counts[filt]
-    std = np.sqrt(w2counts-wcounts**2)
-    if(geomean):
-        wcounts = np.exp(wcounts)
-        std = np.exp(std)
-
-    return wcounts,std
-
-
-#given a list of measurements (say from different replicates) of q1s and q2 datas, average q2 in terms of q1s. 
-def binnedavg_list(q1list,q2list,q1bins=None):
-    if(q1bins is None):
-        mmax = np.max([np.max(q1) for q1 in q1list])
-        mmin = np.min([np.min(q1) for q1 in q1list])
-        q1bins = np.linspace(mmin,mmax) 
-    q1binc= binCenters(q1bins)#just to get the size right. 
-    q1_counts,q1_wcounts,q1_w2counts = np.zeros(shape = q1binc.shape),np.zeros(shape = q1binc.shape),np.zeros(shape = q1binc.shape)
-    for q1,q2 in zip(q1list,q2list):
-        binnedavg_accumulate(q1bins,q1,q2,q1_counts,q1_wcounts,q1_w2counts)
-    q2_q1,q2_q1_std = binnedavg_compute(q1_counts,q1_wcounts,q1_w2counts)
-    return q1bins,q2_q1,q2_q1_std
-
-#from a pair of lists of dataset, compute averages in terms of each other? 
-def paired_binnedavg(q1list,q2list,q1bins=None,q2bins=None):
-    return binnedavg_list(q1list,q2list,q1bins),binnedavg_list(q2list,q1list,q2bins)#(q1bins,q2_q1,q2_q1_std), (q2bins,q1_q2,q1_q2_std) # first, returns q2 as a function of q1, then returns q1 as a function of q2
-
-
 def plot_paired(strains,stresses,src=plt,skipplot=False, **kwargs):
     #assume structure as:
     xs = np.zeros(strains.size)
@@ -228,8 +177,6 @@ def add_inset(text,horizontalalignment='left',ax=matplotlib.pyplot,**kwargs):
         xloc = 0.98
     return ax.text(xloc,.95,text,transform=ax.transAxes, horizontalalignment=horizontalalignment, **kwargs)
 
-def get_param_array_from_dictionaries(list_of_dictionaries, param_name):
-    return np.unique(sorted([dic[param_name] for dic in list_of_dictionaries]))
 
 
 def draw_brace(ax, span, position, text, text_pos, brace_scale=1.0, beta_scale=300., rotate=False, rotate_text=False):
