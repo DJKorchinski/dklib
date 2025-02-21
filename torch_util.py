2bb2a234f79afd311888ca35eb6fbdd8108c2883
Daniel James Korchinski <danielkorchinski@gmail.com>
2025-02-21 15:04:43 +0100

    added unique_tensors() function using my hash code.

--- torch_util.py
+++ torch_util.py
@@ -39,9 +39,37 @@ def hash_tensor(tensor: torch.Tensor) -> int:
     Compute the hash of a PyTorch tensor.
 
     Args:
-        tensor (torch.Tensor): The input tensor.
+        tensor (torch.Tensor): The input tensor. (might break if tensor is on gpu)
 
     Returns:
         int: The hash of the tensor.
     """
     return hash(tensor.numpy().tobytes())
+
+def unique_tensors(tensors: list[torch.Tensor],initial_hashes:set|None = None,return_hashes:bool=False) -> list[torch.Tensor]|tuple[list[torch.Tensor],set]:
+    """
+    Find the unique elements in a list of PyTorch tensors.
+
+    Args:
+        tensors (list[torch.Tensor]): The list of input tensors.
+        initial_hashes (set | None, optional): A set of initial hashes to disallow. Defaults to None. Useful if adding additional tensors to an existing set.
+        return_hashes (bool, optional): If True, returns the set of unique hashes along with the unique tensors. Defaults to False.
+
+    Returns:
+        list[torch.Tensor]: The unique tensors in the input list.
+        tuple[list[torch.Tensor], set]: If return_hashes is True, returns a tuple containing the unique tensors and the set of unique hashes.
+    """
+    unique_tensors = []
+    if(initial_hashes is not None):
+        unique_hashes = initial_hashes
+    else:
+        unique_hashes = set()
+    for tensor in tensors:
+        tensor_hash = hash_tensor(tensor)
+        if tensor_hash not in unique_hashes:
+            unique_hashes.add(tensor_hash)
+            unique_tensors.append(tensor)
+    if(return_hashes):
+        return unique_tensors, unique_hashes
+    else:
+        return unique_tensors
