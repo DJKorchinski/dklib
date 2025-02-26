6cf69dcef068b70fc3583a2c4beb943e87337d8a
Daniel James Korchinski <danielkorchinski@gmail.com>
2025-02-26 12:14:43 +0100

    added print_device_info(device) to prettily format some stats about the current device.

--- torch_util.py
+++ torch_util.py
@@ -73,3 +73,13 @@ def unique_tensors(tensors: list[torch.Tensor],initial_hashes:set|None = None,re
         return unique_tensors, unique_hashes
     else:
         return unique_tensors
+
+def print_device_info(device: torch.device|str):
+    device = torch.device(device)
+    print("Using device:", device, "\n")
+    # Additional Info when using cuda
+    if device.type == "cuda":
+        print(torch.cuda.get_device_name(0))
+        print("Memory Usage:")
+        print("Allocated:", round(torch.cuda.memory_allocated(0) / 1024**3, 1), "GB")
+        print("Cached:   ", round(torch.cuda.memory_reserved(0) / 1024**3, 1), "GB")
